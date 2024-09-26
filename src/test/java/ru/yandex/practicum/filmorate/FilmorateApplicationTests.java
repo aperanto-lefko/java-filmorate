74426bdf1a01cdc14ed2fbd9c53c253823a8be5e
aperanto lefko <polarster@yandex.ru>
2024-09-26 18:48:44 +0300

    Добавлены пробелы

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -16,7 +16,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 class FilmorateApplicationTests {
     static FilmController filmController = new FilmController();
-static UserController userController = new UserController();
+    static UserController userController = new UserController();
+
     @Test
     public void validateFilmDate() {
         Film validFilm = new Film();
@@ -25,14 +26,13 @@ static UserController userController = new UserController();
                 "Метод работает некорректно");
         assertTrue(e.getMessage().contains("Дата релиза не может быть раньше 28.12.1985"));
     }
-@Test
-   public void validateUserLogin() {
+
+    @Test
+    public void validateUserLogin() {
         User validUser = new User();
         validUser.setLogin("name user");
-        Exception e = assertThrows(ValidationException.class,() -> userController.checkLoginField(validUser),
+        Exception e = assertThrows(ValidationException.class, () -> userController.checkLoginField(validUser),
                 "Метод работает некорректно");
         assertTrue(e.getMessage().contains("Поле логин не может содержать пробелы"));
-}
-
-
+    }
 }
