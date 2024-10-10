f5f75d2081b5fef564449e42b5ab2bdaac9cedb0
aperanto lefko <polarster@yandex.ru>
2024-10-11 00:51:05 +0300

    Удален пробел

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorResponse.java
@@ -3,11 +3,12 @@ package ru.yandex.practicum.filmorate.exception;
 import lombok.Getter;
 
 
-    public class ErrorResponse {
-        @Getter
-        String error;
-        public ErrorResponse(String error) {
-            this.error = error;
-        }
+public class ErrorResponse {
+    @Getter
+    String error;
+
+    public ErrorResponse(String error) {
+        this.error = error;
     }
+}
 
